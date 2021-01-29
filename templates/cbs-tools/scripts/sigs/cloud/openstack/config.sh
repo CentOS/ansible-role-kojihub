efa172775f7a027b2889a23bc2a813d2de05f3d2
Thomas Oulevey <thomas.oulevey@cern.ch>
2021-01-29 15:02:15 +0100

    Set TESTING_TAG_INHERITED to true for cloud/openstack

--- templates/cbs-tools/scripts/sigs/cloud/openstack/config.sh
+++ templates/cbs-tools/scripts/sigs/cloud/openstack/config.sh
@@ -1,3 +1,4 @@
  BUILDROOT_DEFAULT="openstack-macros bash bzip2 coreutils cpio diffutils redhat-release findutils gawk gcc gcc-c++ grep gzip info make patch redhat-rpm-config rpm-build sed unzip util-linux-ng git which buildsys-tools xz"
  COMMON_INHERITANCE=false
  HA_REPO_ENABLED=true
+ TESTING_TAG_INHERITED=true
